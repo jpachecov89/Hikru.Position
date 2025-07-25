0fc1e44d5b00cb6817e34c0c217a780bc80f1958
Joel Pacheco Vilcapoma <jpachecov89@gmail.com>
2025-07-25 16:08:30 -0500

    Swagger for Development

--- Hikru.Position.Backend/Hikru.Position.Backend.Api/Program.cs
+++ Hikru.Position.Backend/Hikru.Position.Backend.Api/Program.cs
@@ -35,9 +35,12 @@ namespace Hikru.Position.Backend.Api
             var app = builder.Build();
 
 			// Configure the HTTP request pipeline.
-			app.UseSwagger();
-			app.UseSwaggerUI();
-
+            if (app.Environment.IsDevelopment())
+            {
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+			
 			app.UseHttpsRedirection();
             app.UseAuthorization();
             app.MapControllers();
